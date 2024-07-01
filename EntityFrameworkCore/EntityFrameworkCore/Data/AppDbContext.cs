b5ceb92f4b1ecf9e67cdeecb600c6394201b1157
imsanjaysc <sschouhan4@gmail.com>
2024-07-01 12:39:08 +0530

    Add database

---
+++ EntityFrameworkCore/EntityFrameworkCore/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameworkCore.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
